fe80013a93db35454c5991312677ae1b33dc1809
s.meleychuk@yandex.ru <s.meleychuk@yandex.ru>
2025-01-19 01:50:03 +0300

    йцу

--- Philadelphus.Business/Helpers/InfrastructureConverters/AttributeValueInfrastructureConverter.cs
+++ Philadelphus.Business/Helpers/InfrastructureConverters/AttributeValueInfrastructureConverter.cs
@@ -40,32 +40,5 @@ namespace Philadelphus.Business.Helpers.InfrastructureConverters
             }
             return result;
         }
-
-
-
-
-
-
-        internal override IEnumerable<IDbEntity> BusinessToDbEntityCollection(IEnumerable<IMainEntity> businessEntities)
-        {
-            var result = new List<DbAttributeValue>();
-            foreach (var businessEntity in businessEntities)
-            {
-                var entity = (DbAttributeValue)BusinessToDbMainProperties(businessEntity);
-                result.Add(entity);
-            }
-            return result;
-        }
-
-        internal override IEnumerable<IMainEntity> DbToBusinessEntityCollection(IEnumerable<IDbEntity> dbEntities)
-        {
-            var result = new List<AttributeValue>();
-            foreach (var dbEntity in dbEntities)
-            {
-                var entity = (AttributeValue)DbToBusinessMainProperties(dbEntity);
-                result.Add(entity);
-            }
-            return result;
-        }
     }
 }
