6fa5fde6a4cabe969fe82c60ce12146311531bfd
s.meleychuk@yandex.ru <s.meleychuk@yandex.ru>
2025-03-03 00:21:00 +0300

    Переработка интерфейса, разделение на самостоятельные контролы

--- Philadelphus.WpfApplication/Philadelphus.WpfApplication/Views/Controls/RepositoryExplorer.xaml.cs
+++ Philadelphus.WpfApplication/Philadelphus.WpfApplication/Views/Controls/RepositoryExplorer.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Philadelphus.WpfApplication.Controls
+namespace Philadelphus.WpfApplication.Views.Controls
 {
     /// <summary>
     /// Логика взаимодействия для RepositoryExplorer.xaml
