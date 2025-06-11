2239cee7e8cef5e050118554b80e4f027da96f55
SergeyMeley <s.meleychuk@yandex.ru>
2025-06-12 00:08:40 +0300

    Какие-то изменения

---
+++ Philadelphus.WpfApplication/Philadelphus.WpfApplication/Converters/EntityTypeConverter.cs
@@ -0,0 +1,24 @@
+﻿using Philadelphus.Business.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Philadelphus.WpfApplication.Converters
+{
+    class EntityTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return EntityTypes.Root.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
