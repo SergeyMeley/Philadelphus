71110291f65a6c96cc8f38233b86b8577b89cbaa
SergeyMeley <s.meleychuk@yandex.ru>
2026-01-26 20:52:52 +0300

    Добавил интерфейс работы со строками подключения. Доработал интерфейс и логику работы с настроечными файлами и строками подключения.

---
+++ Philadelphus.Core.Domain/Mapping/DomainMappingProfile.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Philadelphus.Core.Domain.Entities.Infrastructure.DataStorages;
+using Philadelphus.Core.Domain.Entities.MainEntities;
+using Philadelphus.Core.Domain.Entities.MainEntityContent.Properties;
+using Philadelphus.Infrastructure.Persistence.Common.Enums;
+using Philadelphus.Infrastructure.Persistence.Entities.MainEntities;
+using Philadelphus.Infrastructure.Persistence.Entities.MainEntityContent.Properties;
+
+namespace Philadelphus.Core.Domain.Mapping
+{
+    /// <summary>
+    /// Профиль маппинга для Автомаппера
+    /// </summary>
+    public class DomainMappingProfile : Profile
+    {
+        /// <summary>
+        /// Профиль маппинга для Автомаппера
+        /// </summary>
+        public DomainMappingProfile()
+        {
+            //// Entity → DTO
+            //CreateMap<AuditInfo, AuditInfoDto>();
+
+            //// DTO → Entity (для создания)
+            //CreateMap<CreateAuditInfoDto, AuditInfo>()
+            //    .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            //    .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+            //    .ForMember(dest => dest.UpdatedBy, opt => opt.Ignore())
+            //    .ForMember(dest => dest.ContentUpdatedAt, opt => opt.Ignore())
+            //    .ForMember(dest => dest.ContentUpdatedBy, opt => opt.Ignore())
+            //    .ForMember(dest => dest.IsDeleted, opt => opt.Ignore())
+            //    .ForMember(dest => dest.DeletedAt, opt => opt.Ignore())
+            //    .ForMember(dest => dest.DeletedBy, opt => opt.Ignore());
+
+            // Модель бизнес-слоя → Entity инфраструктуры
+            CreateMap<AuditInfoModel, AuditInfo>()
+                .ReverseMap();
+
+            CreateMap<TreeRepositoryModel, TreeRepository>()
+            .ReverseMap()
+            .ConstructUsing((src, dst) =>
+            {
+                // Специальная логика создания
+                return new TreeRepositoryModel(
+                    uuid: src.Uuid,
+                    dataStorage: GetDataStorage(src.OwnDataStorageUuid), //TODO: ПЕРЕДЕЛАТЬ КОСТЫЛЬ
+                    dbEntity: src
+                );
+            });
+        }
+
+        private IDataStorageModel GetDataStorage(Guid uuid)
+        {
+            var builder = new DataStorageBuilder()
+                    .SetGeneralParameters("test", "test", uuid, InfrastructureTypes.PostgreSqlEf, isDisabled: false);
+            return builder.Build();
+        }
+    }
+}
---
+++ Philadelphus.Presentation.Wpf.UI/Mapping/ViewModelsMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Philadelphus.Core.Domain.Configurations;
+using Philadelphus.Presentation.Wpf.UI.ViewModels.EntitiesVMs.SettingsContainersVMs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Philadelphus.Presentation.Wpf.UI.Mapping
+{
+    public class ViewModelsMappingProfile : Profile
+    {
+        public ViewModelsMappingProfile()
+        {
+            CreateMap<ConnectionStringContainer, ConnectionStringContainerVM>().ReverseMap();
+        }
+    }
+}
