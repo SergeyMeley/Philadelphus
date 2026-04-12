89aa0b1e8e477082c7da12cbb3da111ad22cada5
SergeyMeley <s.meleychuk@yandex.ru>
2026-04-12 08:00:05 +0300

    Добавил ViewModelBase.OnPropertyChangedRecursive().

--- Philadelphus.Presentation.Wpf.UI/ViewModels/ViewModelBase.cs
+++ Philadelphus.Presentation.Wpf.UI/ViewModels/ViewModelBase.cs
@@ -10,6 +10,44 @@ namespace Philadelphus.Presentation.Wpf.UI.ViewModels
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        protected internal virtual void OnPropertyChangedRecursive(HashSet<object> visited = null)
+        {
+            visited ??= new HashSet<object>();
+
+            if (!visited.Add(this))
+                return;
+
+            OnPropertyChanged(null);
+
+            foreach (var property in GetType().GetProperties())
+            {
+                // пропускаем индексаторы
+                if (property.GetIndexParameters().Length > 0)
+                    continue;
+
+                var value = property.GetValue(this);
+
+                if (value == null)
+                    continue;
+
+                // одиночная ViewModel
+                if (value is ViewModelBase vm)
+                {
+                    vm.OnPropertyChangedRecursive(visited);
+                }
+                // коллекция
+                else if (value is System.Collections.IEnumerable enumerable)
+                {
+                    foreach (var item in enumerable)
+                    {
+                        if (item is ViewModelBase childVm)
+                        {
+                            childVm.OnPropertyChangedRecursive(visited);
+                        }
+                    }
+                }
+            }
+        }
         protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
         {
             if (Equals(field, value)) return false;
