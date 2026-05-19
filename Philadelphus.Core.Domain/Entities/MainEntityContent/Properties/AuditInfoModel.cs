7c85f3872d92c4e9a3612a21e6801d74e2bc740c
SergeyMeley <s.meleychuk@yandex.ru>
2026-05-19 23:12:34 +0300

    Добавил атрибуты свойствам аудита.

--- Philadelphus.Core.Domain/Entities/MainEntityContent/Properties/AuditInfoModel.cs
+++ Philadelphus.Core.Domain/Entities/MainEntityContent/Properties/AuditInfoModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Philadelphus.Core.Domain.Entities.MainEntityContent.Properties
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Philadelphus.Core.Domain.Entities.MainEntityContent.Properties
 {
     /// <summary>
     /// Информация для аудите
@@ -8,46 +10,55 @@
         /// <summary>
         /// Когда создал
         /// </summary>
+        [Display(Name = "Когда создал", Description = "Когда создал")]
         public DateTime CreatedAt { get; set; }
 
         /// <summary>
         /// Кто создал
         /// </summary>
+        [Display(Name = "Кто создал", Description = "Кто создал")]
         public string CreatedBy { get; set; }
 
         /// <summary>
-        /// Когда обновил
+        /// Когда изменил
         /// </summary>
+        [Display(Name = "Когда изменил", Description = "Когда изменил")]
         public DateTime? UpdatedAt { get; set; }
 
         /// <summary>
-        /// Кто обновил
+        /// Кто изменил
         /// </summary>
+        [Display(Name = "Кто изменил", Description = "Кто изменил")]
         public string? UpdatedBy { get; set; }
 
         /// <summary>
-        /// Когда обновил содержимое
+        /// Когда изменил содержимое
         /// </summary>
+        [Display(Name = "Когда изменил содержимое", Description = "Когда изменил содержимое")]
         public DateTime? ContentUpdatedAt { get; set; }
 
         /// <summary>
-        /// Кто обновил содержимое
+        /// Кто изменил содержимое
         /// </summary>
+        [Display(Name = "Кто изменил содержимое", Description = "Кто изменил содержимое")]
         public string? ContentUpdatedBy { get; set; }
 
         /// <summary>
         /// Удалено
         /// </summary>
+        [Display(Name = "Удалено", Description = "Удалено")]
         public bool IsDeleted { get; set; }
 
         /// <summary>
         /// Когда удалил
         /// </summary>
+        [Display(Name = "Когда удалил", Description = "Когда удалил")]
         public DateTime? DeletedAt { get; set; }
 
         /// <summary>
         /// Кто удалил
         /// </summary>
+        [Display(Name = "Кто удалил", Description = "Кто удалил")]
         public string? DeletedBy { get; set; }
     }
 }
