f92e0271e34b56de4921a27a62f71b1760a836db
SergeyMeley <s.meleychuk@yandex.ru>
2026-06-14 23:17:18 +0300

    test(presentation): тест-проект Philadelphus.Tests.Presentation (Этап 3)

---
+++ Philadelphus.Tests.Presentation/Converters/Logic/LastLaunchToDaysAgoLogicTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Philadelphus.Presentation.Converters.Logic;
+
+namespace Philadelphus.Tests.Presentation.Converters.Logic
+{
+    public class LastLaunchToDaysAgoLogicTests
+    {
+        [Fact]
+        public void Convert_Null_Is_NeverLaunched()
+            => LastLaunchToDaysAgoLogic.Convert(null).Should().Be("никогда не запускался");
+
+        [Fact]
+        public void Convert_NonDate_Is_NeverLaunched()
+            => LastLaunchToDaysAgoLogic.Convert("not a date").Should().Be("никогда не запускался");
+
+        [Fact]
+        public void Convert_FutureDate_Is_InFuture()
+            => LastLaunchToDaysAgoLogic.Convert(DateTime.Now.AddDays(2)).Should().Be("в будущем");
+
+        [Theory]
+        [InlineData(1, "1 день назад")]
+        [InlineData(2, "2 дня назад")]
+        [InlineData(5, "5 дней назад")]
+        [InlineData(11, "11 дней назад")]
+        [InlineData(21, "21 день назад")]
+        public void Convert_PastDate_Uses_Russian_Plural(int daysAgo, string expected)
+            => LastLaunchToDaysAgoLogic.Convert(DateTime.Now.AddDays(-daysAgo - 0.5)).Should().Be(expected);
+    }
+}
---
+++ Philadelphus.Tests.Presentation/Converters/Logic/UtcToLocalTimeLogicTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Philadelphus.Presentation.Converters.Logic;
+
+namespace Philadelphus.Tests.Presentation.Converters.Logic
+{
+    public class UtcToLocalTimeLogicTests
+    {
+        [Fact]
+        public void Convert_Iso8601Utc_Is_Timezone_Independent()
+        {
+            var utc = new DateTime(2026, 4, 2, 15, 30, 45, DateTimeKind.Utc);
+
+            UtcToLocalTimeLogic.Convert(utc, "Iso8601Utc").Should().Be("2026-04-02T15:30:45Z");
+        }
+
+        [Fact]
+        public void Convert_NonDateTime_Returns_Same_Value()
+            => UtcToLocalTimeLogic.Convert("not a date", "Iso8601").Should().Be("not a date");
+    }
+}
