c0bdcc474efef40f795168640ec250d6d053adb7
SergeyMeley <s.meleychuk@yandex.ru>
2026-07-21 13:47:10 +0300

    feat(avalonia): добавить popup поиска значения атрибута

---
+++ Philadelphus.Presentation.Avalonia/Views/Controls/AttributeValueLookupPopup.axaml.cs
@@ -0,0 +1,17 @@
+using global::Avalonia.Controls.Primitives;
+
+namespace Philadelphus.Presentation.Avalonia.Views.Controls;
+
+/// <summary>
+/// Popup расширенного поиска значения одиночного пользовательского атрибута.
+/// </summary>
+public partial class AttributeValueLookupPopup : Popup
+{
+    /// <summary>
+    /// Инициализирует popup расширенного поиска.
+    /// </summary>
+    public AttributeValueLookupPopup()
+    {
+        InitializeComponent();
+    }
+}
